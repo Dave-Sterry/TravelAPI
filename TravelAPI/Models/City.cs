84d9454929ec3db52c2a4c91a08f86ad047d76db
David Sterry <sterry.david@gmail.com>
2021-01-19 14:32:42 -0800

    add validation to city model
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- TravelAPI/Models/City.cs
+++ TravelAPI/Models/City.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TravelAPI.Models
 {
     public class City
     {
         public int CityId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
         public string Attraction { get; set; }
+        [Required]
+        [Range(1, 10, ErrorMessage = "Rating must be between 1-10")]
         public int Rating { get; set; }
     }
 }
