5153e5191be23b9a4896c973ca90b6772cca46c2
David Sterry <sterry.david@gmail.com>
2021-01-19 11:27:20 -0800

    add get, post, delete to cities controller
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- TravelAPI/Controllers/CitiesController.cs
+++ TravelAPI/Controllers/CitiesController.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TravelAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TravelAPI.Controllers
 {
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CitiesController : ControllerBase 
     {
@@ -25,9 +26,35 @@ namespace TravelAPI.Controllers
 
         //POST api/cities
         [HttpPost]
-        public void Post([FromBody]City city)
+        public void Post([FromBody] City city)
         {
             _db.Cities.Add(city);
+            _db.SaveChanges();
+        }
+
+        //GET api/cities/5
+        [HttpGet("{id}")]
+        public ActionResult<City> Get(int id)
+        {
+            return _db.Cities.FirstOrDefault(entry => entry.CityId == id);
+        }
+
+        // PUT api/cities/5
+        // Remember to restart server after each route addition to prevent 400 errors in Postman
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] City city)
+        {
+            city.CityId = id;
+            _db.Entry(city).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        // DELETE api/cities/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var cityToDelete = _db.Cities.FirstOrDefault(entry => entry.CityId == id);
+            _db.Cities.Remove(cityToDelete);
             _db.SaveChanges();
         }
     }
